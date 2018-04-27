2f5de84498f95fdc6c17dac79ea21d2c8d78e1a0
michael-lowe-nz <mikkeyfluttie@gmail.com>
2018-04-28 00:50:47 +1200

    Setup webpack copying

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
+import './index.scss';
+
 console.log('Griswald Glumboonie')
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,39 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
+  mode: 'development',
+  watch: true,
   entry: './src/index.js',
   output: {
       path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
-  }
+  },
+  module: {
+    rules: [{
+        test: /\.scss$/,
+        use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+        }, {
+            loader: "css-loader" // translates CSS into CommonJS
+        }, {
+            loader: "sass-loader" // compiles Sass to CSS
+        }]
+    }]
+  },
+  node: {
+    console: false,
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty'
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: './src/index.html',
+        to: './index.html',
+      }
+    ])
+  ]
 };
