52a5baf59b2befb3a8a9f912a423b76e614f4f83
michael-lowe-nz <mikkeyfluttie@gmail.com>
2018-05-02 00:16:55 +1200

    Cheesy homepage

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-import './index.scss';
+import './index.scss'
 
 console.log('Griswald Glumboonie')
+
+function waitALittle() {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, 1000);
+  })
+}
+
+async function main () {
+  await waitALittle()
+  console.log('Finito')
+}
+
+main()
