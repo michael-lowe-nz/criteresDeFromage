921d34239e05d8e59ddfc392704290c7b57c2f99
michael-lowe-nz <mikkeyfluttie@gmail.com>
2018-05-10 00:44:43 +1200

    Add some questions in

--- src/routes/questions/index.js
+++ src/routes/questions/index.js
@@ -2,28 +2,38 @@ import { h, Component } from 'preact';
 import style from './style.scss';
 
 export default class Questions extends Component {
+  state = {mike: 'helsinki'}
+
+  handleAnswer = (question, answer) => {
+    this.setState({
+      [question]: answer,
+      rating: 50
+    })
+  }
+
   render() {
     return (
       <section class="section main">
         <div class="container">
           <div class="columns is-multiline">
-
-
-
-
-            <div class="column is-12">
-              <label class="label">Is the situation the cheese is in</label>
-              <div class="field is-grouped">
-                <p class="control">
-                  <a class="button is-outlined is-light">a Pizza</a>
-                </p>
-                <p class="control">
-                  <a class="button is-outlined is-light">a Cheese Scone</a>
-                </p>
-              </div>
-            </div>
-
-
+            <Question
+              onClick={this.handleAnswer}
+              question={"What cheese is it?"}
+              answerState={this.state}
+              answers={["Parmesan", "Blue Cheese", "Mozarella", "Other"]}
+            />
+            <Question
+              onClick={this.handleAnswer}
+              question={"What temperature is the cheese?"}
+              answerState={this.state}
+              answers={["Hot", "Cold"]}
+            />
+            <Question
+              onClick={this.handleAnswer}
+              question={"What situation is the cheese in?"}
+              answerState={this.state}
+              answers={["On a pizza", "In a burger", "In a sandwich", "on top of something"]}
+            />
           </div>
         </div>
       </section>
@@ -39,7 +49,11 @@ class Question extends Component {
         <div class="field is-grouped">
         {this.props.answers.map(answer => (
           <p class="control">
-            <a class="button is-outlined is-light">answer</a>
+            <a
+              class={`button is-dark ${this.props.answerState[this.props.question] !== answer && 'is-outlined'}`}
+              onClick={() => this.props.onClick(this.props.question, answer)}>
+              {answer}
+            </a>
           </p>
         ))}
         </div>
@@ -47,3 +61,4 @@ class Question extends Component {
     )
   }
 }
+
