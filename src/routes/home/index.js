c5d4e0173046c1de0678e60ce0c2423b6ae6c7f0
michael-lowe-nz <mikkeyfluttie@gmail.com>
2018-05-09 23:26:05 +1200

    Sassy setup

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,13 +1,23 @@
 import { h, Component } from 'preact';
-import style from './style';
+import './style.scss';
 
 export default class Home extends Component {
-	render() {
-		return (
-			<div class={style.home}>
-				<h1>Home</h1>
-				<p>This is the Home component.</p>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <section class="section main">
+        <div class="container">
+          <h1 class="title has-text-white">The Cheese Rules</h1>
+          <p class="subtitle has-text-light">a Michael Lowe preference project</p>
+          <div class="field is-grouped">
+            <p class="control">
+              <a class="button is-primary">Begin</a>
+            </p>
+            <p class="control">
+              <a rel="noopener noreferrer" target="_blank" href="https://github.com/michael-lowe-nz/criteresDeFromage" class="button is-outlined is-primary">Wtf lol</a>
+            </p>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
